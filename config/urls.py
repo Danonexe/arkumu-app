786601df138b910fac714076f2ae74d0b545f07d
Daniel Hernandez <dhemande@uni-koeln.de>
2025-06-10 12:44:40 +0200

    documentation

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,7 @@ from django.conf.urls.i18n import i18n_patterns
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/prueba.html"), name="home"),
     path("components/", TemplateView.as_view(template_name="pages/components.html"), name="home"),
+    path("documentation/", TemplateView.as_view(template_name="pages/documentation.html"), name="home"),
     path("projekt/", TemplateView.as_view(template_name="pages/projekt.html"), name="home"),
     path(
         "about/",
