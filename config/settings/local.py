bae38b24a7652b17165e9af20e4fce28443805df
Francisco Mondaca <mondaca@uni-koeln.de>
2025-06-25 14:34:18 +0200

    settings up

--- config/settings/local.py
+++ config/settings/local.py
@@ -16,6 +16,10 @@ SECRET_KEY = env(
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "dev.arkumu.uni-koeln.de"]  # noqa: S104
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://dev.arkumu.uni-koeln.de",
+]
+
 # CACHES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#caches
